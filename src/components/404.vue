8b7d78061f5e9d07b4e6b147f84e91a43d1e6c0e
mengdehua@vipkid.com.cn <mengdehua@mengdehuavipkidcomcndeMacBook-Pro.local>
2018-08-09 19:35:40 +0800

    rewrite window

---
+++ src/components/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="test" >
+    this is 404 component
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {}
+  },
+  methods:{
+    r(path){
+      this.$router.push({path:path})
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import App from '../App'
 const aaa = ()=>import('../components/aaa')
 const bbb = ()=>import('../components/bbb')
 const ccc = ()=>import('../components/ccc')
+const r404 = ()=>import('../components/404')
 //import aaa from '../components/aaa'
 //import bbb from '../components/bbb'
 //import ccc from '../components/ccc'
@@ -21,8 +22,8 @@ const routers = [
 				]
 			},
 			{
-				path: ':bbb',
-				component: bbb
+				path: ':id',
+				component: r404
 			},
 			{
 				path: 'ccc',
